4d2473c3af44c844ec66e95cc569c1c1b48faf2e
khadada <lkhadada@gmail.com>
2021-11-30 07:29:15 +0100

    I add some features to avode redouble state in our list of state and to tell the player when he/she entre guessed state to try another one and print the guessed list of state

--- us-states-game-start/main.py
+++ us-states-game-start/main.py
@@ -6,9 +6,16 @@ img = 'blank_states_img.gif'
 screen.addshape(img)
 turtle.shape(img)
 state = State('50_states.csv')
-while True:
+guessed_states = []
+while len(guessed_states) < 50:
     answer = screen.textinput(title=f'score {state.score}/{len(state.all_states)}', prompt="What's the state: ").title()
     if state.is_state_exist(answer):
-        position = state.get_state_position(answer)
-        state.print_state(position, answer)
-        state.score_up()
+        if answer not in guessed_states:
+            guessed_states.append(answer)
+            position = state.get_state_position(answer)
+            state.print_state(position, answer)
+            state.score_up()
+        else:
+            print('You guessed this state. try another')
+            print('You guessed this state:')
+            print(guessed_states)
